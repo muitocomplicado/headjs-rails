683de4ef3b21e25e1206255315f6fefb5a720bf5
David Bittencourt <muitocomplicado@gmail.com>
2010-12-07 12:37:12 -0200

    Updated URLs of the raw Head JS files in the generator since they were moved to a subdirectory.

--- lib/generators/headjs/install/install_generator.rb
+++ lib/generators/headjs/install/install_generator.rb
@@ -23,8 +23,8 @@ module Headjs
     private
 
       def get_headjs(version)
-        get "https://github.com/headjs/headjs/raw/#{version}/head.js",     "public/javascripts/head.js"
-        get "https://github.com/headjs/headjs/raw/#{version}/head.min.js", "public/javascripts/head.min.js"
+        get "https://github.com/headjs/headjs/raw/#{version}/dist/head.js",     "public/javascripts/head.js"
+        get "https://github.com/headjs/headjs/raw/#{version}/dist/head.min.js", "public/javascripts/head.min.js"
       end
 
     end
