47a9fc6a34ebcb5744b80abdc600d54aa2a221ae
Matthew Schulkind <mschulkind@gmail.com>
2012-08-06 19:13:32 -0400

    Refactor the tag stuff slightly to allow more custom loading.

--- lib/headjs-rails/tag_helper.rb
+++ lib/headjs-rails/tag_helper.rb
@@ -3,17 +3,21 @@ module Headjs
   module TagHelper
 
     def headjs_include_tag(*sources)
-      keys = []
       content_tag :script, { :type => Mime::JS }, false do
-        "head.js( #{javascript_include_tag(*sources).scan(/src="([^"]+)"/).flatten.map { |src| 
-          key = URI.parse(src).path[%r{[^/]+\z}].gsub(/\.js$/,'').gsub(/\.min$/,'')
-          while keys.include?(key) do
-            key += '_' + key
-          end
-          keys << key
-          "{ '#{key}': '#{src}' }"
-        }.join(', ')} );"
+        headjs_include_js(*sources)
       end
+    end
+
+    def headjs_include_js(*sources)
+      keys = []
+      "head.js( #{javascript_include_tag(*sources).scan(/src="([^"]+)"/).flatten.map { |src| 
+        key = URI.parse(src).path[%r{[^/]+\z}].gsub(/\.js$/,'').gsub(/\.min$/,'')
+        while keys.include?(key) do
+          key += '_' + key
+        end
+        keys << key
+        "{ '#{key}': '#{src}' }"
+      }.join(', ')} );"
     end
 
   end
