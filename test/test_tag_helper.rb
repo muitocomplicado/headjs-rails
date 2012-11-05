e08974d404381e87a54ceca9a9d75449231e889e
David Bittencourt <muitocomplicado@gmail.com>
2012-11-05 10:32:40 -0200

    Fixed test expectations to be the first parameter and fixed issue with how assets with same urls in the include call are not called multiple times by rails anymore. The concatenation of labels works for different urls but it's not pretty or that useful.

--- test/test_tag_helper.rb
+++ test/test_tag_helper.rb
@@ -31,49 +31,46 @@ module Headjs
     context "The Head JS helper include tag" do
     
       should "return a valid script tag" do
-        assert_equal @helpers.headjs_include_tag(jquery), "<script type=\"text/javascript\">head.js( { 'jquery': '#{jquery}' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '#{jquery}' } );</script>", @helpers.headjs_include_tag(jquery)
       end
-    
-      should "concatenate labels to avoid overwriting" do
-        assert_equal @helpers.headjs_include_tag(jquery, jquery), "<script type=\"text/javascript\">head.js( { 'jquery': '#{jquery}' }, { 'jquery_jquery': '#{jquery}' } );</script>"
+      
+      should "concatenate labels to avoid overwriting (not that great)" do
+        assert_equal "<script type=\"text/javascript\">head.js( { 'comments': '/javascripts/products/comments.js' }, { 'comments_comments': '/javascripts/reviews/comments.js' } );</script>", @helpers.headjs_include_tag('products/comments', 'reviews/comments')
       end
       
       should "accept strings for local paths" do
-        assert_equal @helpers.headjs_include_tag('jquery', 'other/rails'), "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/other/rails.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/other/rails.js' } );</script>", @helpers.headjs_include_tag('jquery', 'other/rails')
       end
       
       should "accept :defaults registered expansions" do
-        assert_equal @helpers.headjs_include_tag(:defaults), "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' } );</script>", @helpers.headjs_include_tag(:defaults)
       end
       
       should "accept :defaults mixed with other strings" do
-        assert_equal @helpers.headjs_include_tag(:defaults, 'modernizr'), "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' }, { 'modernizr': '/javascripts/modernizr.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' }, { 'modernizr': '/javascripts/modernizr.js' } );</script>", @helpers.headjs_include_tag(:defaults, 'modernizr')
       end
       
       should "accept :all expansion" do
-        assert_equal @helpers.headjs_include_tag(:all), "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' }, { 'application': '/javascripts/application.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js' }, { 'rails': '/javascripts/rails.js' }, { 'application': '/javascripts/application.js' } );</script>", @helpers.headjs_include_tag(:all)
       end
       
       should "accept a :cache => true parameter" do
         @helpers.config.perform_caching = true
-        assert_equal @helpers.headjs_include_tag(:defaults, :cache => true), "<script type=\"text/javascript\">head.js( { 'all': '/javascripts/all.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'all': '/javascripts/all.js' } );</script>", @helpers.headjs_include_tag(:defaults, :cache => true)
       end
       
       should "accept a :cache => bundle parameter" do
         @helpers.config.perform_caching = true
-        assert_equal @helpers.headjs_include_tag(:defaults, :cache => 'bundle'), "<script type=\"text/javascript\">head.js( { 'bundle': '/javascripts/bundle.js' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'bundle': '/javascripts/bundle.js' } );</script>", @helpers.headjs_include_tag(:defaults, :cache => 'bundle')
       end
       
       should "work with random rails asset ids" do
         ENV["RAILS_ASSET_ID"] = '123456789'
-        assert_equal @helpers.headjs_include_tag(:defaults), "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js?#{ENV["RAILS_ASSET_ID"]}' }, { 'rails': '/javascripts/rails.js?#{ENV["RAILS_ASSET_ID"]}' } );</script>"
+        assert_equal "<script type=\"text/javascript\">head.js( { 'jquery': '/javascripts/jquery.js?#{ENV["RAILS_ASSET_ID"]}' }, { 'rails': '/javascripts/rails.js?#{ENV["RAILS_ASSET_ID"]}' } );</script>", @helpers.headjs_include_tag(:defaults)
       end
       
       should "remove rails asset hashes" do
-        assert_equal(
-          @helpers.headjs_include_tag('/assets/foo-f1d2d2f924e986ac86fdf7b36c94bcdf.js'), 
-          "<script type=\"text/javascript\">head.js( { 'foo': '/assets/foo-f1d2d2f924e986ac86fdf7b36c94bcdf.js' } );</script>"
-        )
+        assert_equal "<script type=\"text/javascript\">head.js( { 'foo': '/assets/foo-f1d2d2f924e986ac86fdf7b36c94bcdf.js' } );</script>", @helpers.headjs_include_tag('/assets/foo-f1d2d2f924e986ac86fdf7b36c94bcdf.js')
       end
       
     end
